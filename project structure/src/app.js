c4db358537675dc0027a7b6a3bde88e86206707a
gitkrishh <msgkrish192@gmail.com>
2025-07-28 23:06:40 +0530

    adding some middleware

--- project structure/src/app.js
+++ project structure/src/app.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+
 
 const app = express()
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+app.use( express.json({limit:"16kb"})) // limit the size of the json file to 16kb
+app.use(express.urlencoded({extended: true, limit:"16kb"})) // This middleware is used to parse incoming requests with URL-encoded payloads — typically sent by HTML forms (application/x-www-form-urlencoded).
+app.use(express.static("public")) //This middleware serves static files from the "public" directory.That means: any files inside public/ — like HTML, CSS, JS, images, fonts — become directly accessible via the browser.
 
 export {app}
--- project structure/src/index.js
+++ project structure/src/index.js
@@ -1,7 +1,13 @@
 import {app} from "./app.js";
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: "./.env"
+})
 
 
-const PORT = 8001
+
+const PORT = process.env.PORT || 8001
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
