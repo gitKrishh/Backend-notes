c2a5bad236e113c3635dfca38c6a318bf27990bb
gitkrishh <msgkrish192@gmail.com>
2025-07-28 23:55:58 +0530

    MongoDB connected

--- project structure/src/app.js
+++ project structure/src/app.js
@@ -10,6 +10,7 @@ app.use(
     })
 )
 
+//common middlewares:
 app.use( express.json({limit:"16kb"})) // limit the size of the json file to 16kb
 app.use(express.urlencoded({extended: true, limit:"16kb"})) // This middleware is used to parse incoming requests with URL-encoded payloads — typically sent by HTML forms (application/x-www-form-urlencoded).
 app.use(express.static("public")) //This middleware serves static files from the "public" directory.That means: any files inside public/ — like HTML, CSS, JS, images, fonts — become directly accessible via the browser.
--- project structure/src/index.js
+++ project structure/src/index.js
@@ -1,13 +1,17 @@
 import {app} from "./app.js";
 import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./.env"
 })
 
-
-
 const PORT = process.env.PORT || 8001
+connectDB()
+.then()
+.catch((error)=> {
+    console.log("MongoDB connection error", err)
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
