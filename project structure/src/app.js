23a3e191cd34a3d585f5da930ca859cf04b281c7
gitkrishh <msgkrish192@gmail.com>
2025-07-29 18:55:29 +0530

    added: healthcheck controller and healthcheck router

--- project structure/src/app.js
+++ project structure/src/app.js
@@ -15,4 +15,34 @@ app.use( express.json({limit:"16kb"})) // limit the size of the json file to 16k
 app.use(express.urlencoded({extended: true, limit:"16kb"})) // This middleware is used to parse incoming requests with URL-encoded payloads — typically sent by HTML forms (application/x-www-form-urlencoded).
 app.use(express.static("public")) //This middleware serves static files from the "public" directory.That means: any files inside public/ — like HTML, CSS, JS, images, fonts — become directly accessible via the browser.
 
+//import routes
+import  healthcheckRouter  from './routes/healthcheck.routes.js'
+
+//routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
+//GENERAL SYNTAX : app.use([path], callbackFunction OR router)
+/*
+
+
+    path (optional): The base URL path.
+
+    callback/router: What should be run when a request hits that path.
+
+    import userRoutes from "./routes/user.routes.js";
+app.use("/api/v1/users", userRoutes);
+
+This tells Express:
+
+    "For anything starting with /api/v1/users, use the userRoutes router."
+
+
+*/ 
+
+
+/*
+This mounts the router on the path /api/v1/healthcheck
+So now any request that starts with /api/v1/healthcheck will go to healthcheck.routes.js for further routing.
+*/
+
 export {app}
